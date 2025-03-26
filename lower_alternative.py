0c8ad6eb8125fc26b649847e38a6d8617d57ec00
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 01:13:13 +0800

    added code for program 3 bathc 6 and no bugs encountered

--- lower_alternative.py
+++ lower_alternative.py
@@ -1,5 +1,18 @@
 #ask user to input a string, contain in user_string
+user_string = input("input a string: ")
+
 #create a for loop that iterates through user_string
-#within for loop, create if statement that evaluates whether a character is upper case
-#if True, use replace() to convert character to lower along with swapcase()
+for i in user_string:
+
+    #within for loop, create if statement that evaluates whether a character is upper case
+    if i.isupper():
+
+        #if True, use replace() to convert character to lower along with swapcase()
+        user_string = user_string.replace(i, i.swapcase())
+
+    #else, continue iteration
+    else:
+        continue
+
 #after iteration, print(user_string)
+print(user_string)
