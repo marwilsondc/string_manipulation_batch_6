ab1a442a80ae3ea0208a72158d2aa3b8e23bb683
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 01:08:55 +0800

    added initial pseudocode for lower_alternative.py

---
+++ lower_alternative.py
@@ -0,0 +1,5 @@
+#ask user to input a string, contain in user_string
+#create a for loop that iterates through user_string
+#within for loop, create if statement that evaluates whether a character is upper case
+#if True, use replace() to convert character to lower along with swapcase()
+#after iteration, print(user_string)
