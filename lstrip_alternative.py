f89bf6086ca41abc5612530d6ac26bae59087dfb
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 00:01:16 +0800

    added initial code for program 1 batch 6

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -1,5 +1,17 @@
 #ask user to enter their full name with several spaces in the beginning, store in variable
+user_string = input("Input your full name with several spaces before it: ")
+
 #create a for loop that iterates through the user_string
-#create an if statement that evaluates whether iteration is a whitespace
-#within if statement, replace whitespace into nothing or ""
-#else, break the whole for loop
+for i in user_string:
+    
+    #create an if statement that evaluates whether iteration is a whitespace
+    if i.isspace(): 
+
+        #within if statement, replace whitespace into nothing or ""
+        new_string = user_string.replace(i, "")
+
+    #else, break the whole for loop
+    else:
+        break
+
+print(new_string)
