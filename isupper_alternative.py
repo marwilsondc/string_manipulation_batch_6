8b5aef874febca893fe98b73d9ca452967a0a3db
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 01:24:34 +0800

    added initial pseudocode for program 4 batch 6

---
+++ isupper_alternative.py
@@ -0,0 +1,8 @@
+#ask user to input a string to be evaluated, contain in user_string
+#initialize return_bool
+#create a for loop to iterate through user_string 
+#within for loop, create if statement of return_bool
+#within initial if statement, create if statement to evaluate whether iteration is uppercase 
+#if True, continue iteration
+#else, set return_bool to False and print return_bool
+#if for loop iterates throughout user_string, print return_bool (display True)
