64f85adb3fff00485e4307fc4304894e3d2e4ad0
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 00:04:37 +0800

    encountered bug in initial code of program 1

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -1,17 +1,21 @@
 #ask user to enter their full name with several spaces in the beginning, store in variable
 user_string = input("Input your full name with several spaces before it: ")
 
+#initialize alpha_flag
+alpha_flag = True
+
 #create a for loop that iterates through the user_string
 for i in user_string:
     
     #create an if statement that evaluates whether iteration is a whitespace
-    if i.isspace(): 
+    if i.isspace() and alpha_flag: 
 
         #within if statement, replace whitespace into nothing or ""
         new_string = user_string.replace(i, "")
 
     #else, break the whole for loop
     else:
+        alpha_flag = False
         break
 
 print(new_string)
