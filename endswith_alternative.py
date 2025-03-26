7031ae3effae87c45564145600ca08de548615a7
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 02:10:46 +0800

    added initial pseudocode for program 5 batch 6

---
+++ endswith_alternative.py
@@ -0,0 +1,12 @@
+#ask user to input a string to be evaluated, contain in user_string
+#ask user to input the condition to evaluate string with, contain in endswith_eval
+#initialize return_bool 
+#initialize comparator_string
+#create a for loop to iterate through user_string
+#within for loop, create if statement to evaluate whether iterations match endswith_eval
+#if True, add iteration to comparator_string
+#within initial if statement, create another if statement that compares comparator_string with endswith_eval
+#if True, set return_bool to True
+#outside initial if statement, continue else statement
+#if True, clear comparator_string and set return_bool to False
+#outside for loop, print return_bool
