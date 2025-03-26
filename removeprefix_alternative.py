7a05791aeedadcf15e3a545541fc00a249e98641
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 00:41:10 +0800

    added initial pseudocode to program 2 batch 6

---
+++ removeprefix_alternative.py
@@ -0,0 +1,9 @@
+#ask user to input a string, contain in a variable
+#ask the user for the prefix that they want to remove, contain in a variable
+#initialize terminate_flag 
+#initialize index_count
+#create a for loop to iterate through user_string
+#within for loop, create if statement to compare iteration and user_string index to user_prefix
+#if true, add the character to compare_prefix
+#if compare_prefix equals user_prefix, replace compare_prefix with "" then print result
+#else, set terminate_flag to false and break
