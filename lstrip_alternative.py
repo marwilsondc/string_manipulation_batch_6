a5b682b6f062e1f4cee504b25ec0fce807076f83
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-26 23:56:10 +0800

    added initial pseudocode for program 1 batch 6

---
+++ lstrip_alternative.py
@@ -0,0 +1,5 @@
+#ask user to enter their full name with several spaces in the beginning, store in variable
+#create a for loop that iterates through the user_string
+#create an if statement that evaluates whether iteration is a whitespace
+#within if statement, replace whitespace into nothing or ""
+#else, break the whole for loop
