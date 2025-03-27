3660b090a2628b1793c142cb8c67dc88630759e1
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 23:06:51 +0800

    added initial pseudocode for program 9 batch 6

---
+++ capitalize_alternative.py
@@ -0,0 +1,10 @@
+#define capitalize(str)
+#within function, initialize final_result, index_count
+#below initialization, create if statement that evaluates whether first iteration is part of the alphabet
+#within if statement, create for loop to iterate through string
+#within for loop, create another if statement that evaluates whether index_count is 0
+#if above statement is True, add the iteration to final_result in uppercase and add 1 to index_count
+#else, another if statement, evaluate whether iteration is alpha
+#if true, add iteration to final_result in lowercase 
+#else, add iteration to final_result
+#else, return string itself
