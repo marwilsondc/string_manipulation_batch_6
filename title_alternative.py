c8c6ed1b165d138714effffb6eb010bffa450a68
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 23:39:14 +0800

    added initial code for program 10 batch 6

--- title_alternative.py
+++ title_alternative.py
@@ -1,10 +1,31 @@
 #define title_alt(str)
-#within function, initialize is_separated = True and final_result
-#create for loop to iterate through str
-#within for loop, create if statement that evaluates whether iteration is alpha
-#within if statement, create another if statement that evaluates whether is_separated is True
-#if above statement is True, add iteration to final_result in uppercase and set is_separated to False
-#within if statement, continue to else statement for when is_separated is False
-#if above statemnt is True, add iteration to final_result in lowercase
-#else is for characters outside alpha, including numbers, whitespaces, and symbols
-#if above statement is True, set is_separated to True
+def title_alt(str):
+
+    #within function, initialize is_separated = True and final_result
+    is_separated = True
+    final_result = ""
+
+    #create for loop to iterate through str
+    for i in str:
+
+        #within for loop, create if statement that evaluates whether iteration is alpha
+        if i.isalpha():
+
+            #within if statement, create another if statement that evaluates whether is_separated is True
+            if is_separated:
+
+                #if above statement is True, add iteration to final_result in uppercase and set is_separated to False
+                final_result += i.upper()
+                is_separated = False
+
+            #within if statement, continue to else statement for when is_separated is False
+            else: 
+
+                #if above statemnt is True, add iteration to final_result in lowercase
+                final_result += i.lower()
+
+        #else is for characters outside alpha, including numbers, whitespaces, and symbols
+        else:
+
+            #if above statement is True, set is_separated to True
+            is_separated = True
