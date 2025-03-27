4f9b719b3a76e21935c2bf188c88269cefeccf5b
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 13:03:50 +0800

    debug: IndexError fix by adding another variable (final_result)

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -4,22 +4,26 @@ user_string = input("input a string: ")
 #initialize index_count
 index_count = 0
 
+final_result = ""
 #create a for loop to iterate through user_string
 for i in user_string:
 
-    #within for loop, create if statement to evaluate if iteration is a space. If True, continue iteration
-    if i.isspace():
-        continue
-
     #elif, evaluate if iteration is lowercase, if true, convert user_string[index_count] to uppercase and add 1 to index_count
-    elif i.islower():
-        user_string = user_string[index_count].upper()
+    if i.islower():
+        final_result += user_string[index_count].upper()
         index_count += 1
+        continue
 
     #elif, evaluate if iteration is uppercase, if true, convert user_string[index_count] to lowercase and add 1 to index_count
     elif i.isupper():
-        user_string = user_string[index_count].lower()
+        final_result += user_string[index_count].lower()
         index_count += 1
+        continue
+    
+    else:
+        index_count += 1
+        final_result += i
+        continue
 
 #print user_string
-print(user_string)
+print(final_result)
