5d84dfc8f283ba0019840da2526f4b80e6e096af
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 12:44:58 +0800

    added initial pseudocode to program 8 batch 6

---
+++ swapcase_alternative.py
@@ -0,0 +1,7 @@
+#ask user to input a string to use swapcase on, store in user_string
+#initialize index_count
+#create a for loop to iterate through user_string
+#within for loop, create if statement to evaluate if iteration is a space. If True, continue iteration
+#elif, evaluate if iteration is lowercase, if true, convert user_string[index_count] to uppercase and add 1 to index_count
+#elif, evaluate if iteration is uppercase, if true, convert user_string[index_count] to lowercase and add 1 to index_count
+#print user_string
