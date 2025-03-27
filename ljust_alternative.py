8aa31ade97bb46535f826e6af9bd448252cd3599
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 11:42:58 +0800

    added initial code for program 6 batch 6

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,4 +1,17 @@
 #define ljust_alt(str, val)
-#create while loop that runs until the length of str reaches val
-#within while loop, add " " or whitespace to str
-#outside while loop, return str
+def ljust_alt(str, val):
+
+    #create while loop that runs until the length of str reaches val
+    while len(str) < val:
+
+        #within while loop, add " " or whitespace to str
+        str += " "
+
+    #outside while loop, return str
+    return str
+
+#tester
+user_string = input("input a string = ")
+other_text = "whitespace ends before this text."
+
+print(ljust_alt(user_string, 20) + other_text)
