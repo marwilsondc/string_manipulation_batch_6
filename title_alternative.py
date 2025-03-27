885d01cf560e0fc2aa113fde27e13f60a8ff95c0
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-27 23:35:20 +0800

    added initial pseudocode for program 10 batch 6

---
+++ title_alternative.py
@@ -0,0 +1,10 @@
+#define title_alt(str)
+#within function, initialize is_separated = True and final_result
+#create for loop to iterate through str
+#within for loop, create if statement that evaluates whether iteration is alpha
+#within if statement, create another if statement that evaluates whether is_separated is True
+#if above statement is True, add iteration to final_result in uppercase and set is_separated to False
+#within if statement, continue to else statement for when is_separated is False
+#if above statemnt is True, add iteration to final_result in lowercase
+#else is for characters outside alpha, including numbers, whitespaces, and symbols
+#if above statement is True, set is_separated to True
